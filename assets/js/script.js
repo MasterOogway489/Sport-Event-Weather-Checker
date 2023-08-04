87506aa5dab793916a0fda107fa2a586987ee4e3
uraniumrainbow <uraniumrainbow@gmail.com>
2023-08-03 20:34:15 -0600

    dynamic logo updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,13 @@
 // 'X-RapidAPI-Host': 'api-american-football.p.rapidapi.com'
 var teamIdEl = document.getElementById("search-input");
 var formEl = document.getElementById("search-form");
+var homeTeamID
+var awayTeamID
+var homeTeamID1
+var homeTeamID2
+var awayTeamID1
+var awayTeamID2
+var date
 var teamID = ["Skip", "Las Vegas Raiders", "Jacksonville Jaguars", "New England Patriots", "New York Giants", "Baltimore Ravens", "Tennessee Titans", "Detroit Lions",
 "Atlanta Falcons", "Cleveland Browns", "Cincinnati Bengals", "Arizona Cardinals", "Philidelphia Eagles", "New York Jets", "San Francisco 49ers",
 "Green Bay Packers", "Chicago Bears", "Kansas City Cheifs", "Washington Commanders", "Carolina Panthers", "Buffalo Bills",
@@ -47,12 +54,22 @@ function getGameInfo(date) {
         console.log(data)
 
         if (data.results === 0) {
-            throw new Exception("No Games Found")
+            throw new Error("No Games Found")
         }
 
-        const homeTeamID = data.response[0].teams.home.id
-        const awayTeamID = data.response[0].teams.away.id
-        TeamImg(homeTeamID, awayTeamID);   
+        var homeTeamID = data.response[0].teams.home.id
+        var awayTeamID = data.response[0].teams.away.id
+        var homeTeamID1 = data.response[1].teams.home.id
+        var awayTeamID1 = data.response[1].teams.away.id
+        var homeTeamID2 = data.response[2].teams.away.id
+        var awayTeamID2 = data.response[2].teams.away.id
+        TeamImg(homeTeamID, awayTeamID);
+        if (data.response[1] !== 0) {
+            otherGames(homeTeamID1, awayTeamID1)
+        };
+        if (data.response[2] !== 0) {
+            otherGames2(homeTeamID2, awayTeamID2)
+        }
     })
 
 }; 
@@ -62,18 +79,34 @@ function TeamImg(homeTeamID, awayTeamID){;
      homeImg.src = 'https://media.api-sports.io/american-football/teams/' + homeTeamID + '.png';
      var homeSrc = document.getElementById("home-team-logo");
      homeSrc.appendChild(homeImg);
+     var home = teamID[homeTeamID];
+     document.getElementById("home").innerHTML = home
 
      var awayImg = document.createElement("img");
      awayImg.src = 'https://media.api-sports.io/american-football/teams/' + awayTeamID + '.png';
      var awaySrc = document.getElementById("away-team-logo");
      awaySrc.appendChild(awayImg)
+     var away = teamID[awayTeamID];
+     document.getElementById("away").innerHTML = away
  };
     
+function otherGames(homeTeam, awayTeam){
+    var home = teamID[homeTeam];
+    var away = teamID[awayTeam];
+    document.createElement("ul")
+    document.getElementById("game2").innerHTML = home + " VS " + away;
+};
+
+function otherGames2(homeTeam, awayTeam){
+    var home = teamID[homeTeam];
+    var away = teamID[awayTeam];
+    document.createElement("ul")
+    document.getElementById("game3").innerHTML = home + " VS " + away;
+};
 //Weather API function
 //need function to fill var latLong
 
-function getWeather() {
-    var latLong = cityWeather[homeTeamID];
+function getWeather(latLong) {
    var requestURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${latLong}?key=WNRU679QQP5CDJZWL8EN8LWH9`;
    var conditionsArray = [];
    var tempArray = [];
@@ -117,7 +150,22 @@ function getWeather() {
     console.log(iconArray);
 };
 
-getWeather();
 
 function makeGameCard() {}
+
+//dropdown menu codeblock
+
+// console.log(window);
+// console.log(window.document);
+
+// var dropdown = document.querySelector('.dropdown');
+// dropdown.addEventListener('click', function(event) {
+//   event.stopPropagation();
+//   dropdown.classList.toggle('is-active');
+//   console.log(dropdown)
+//   console.log(dropdown.dropdown-content)
+//   console.log(dropdown-item)
+
+// });
+
 
